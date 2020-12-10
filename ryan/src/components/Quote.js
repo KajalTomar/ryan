3bf1ad5638037a430c2c01e002ce94f1b5229acf
KajalTomar <kajaltomar09@gmail.com>
2020-12-10 03:03:25 -0600

    im tired

--- ryan/src/components/Quote.js
+++ ryan/src/components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Quote = () => {
 	return(
 		<div className="Quote" style={{display: 'flex', justifyContent: 'center'}}>
-			<h1 className='f2 dim yellow pa5'>Quotes go here</h1>
+			<h1 className='f4 dim yellow pt5 pb0'>Yo what is up over there bud</h1>
 		</div>
 	);
 }
--- ryan/src/components/Streak.js
+++ ryan/src/components/Streak.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import flame from './flame.gif';
 
 const Streak = () => {
-	return
-	(
-
+	return(
+		<div className="Streak" style={{display: 'flex', flexDirection:'column'}}>
+			<p className='f1 yellow mv0 pill b--washed-yellow'>100</p>
+			<img id="flame" src={flame} alt='imagine that there is a gif flame here'/>
+		</div>
 	);
 
 }
