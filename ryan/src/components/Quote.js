cf7f6b7cb393be2ce3055aa9f91fc1a4c14edda7
KajalTomar <kajaltomar09@gmail.com>
2020-12-10 01:47:12 -0600

    trying to add flame

--- ryan/src/components/Quote.js
+++ ryan/src/components/Quote.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Quote = () => {
 	return(
-		<div style={{display: 'flex', justifyContent: 'center'}}>
+		<div className="Quote" style={{display: 'flex', justifyContent: 'center'}}>
 			<h1 className='f2 dim yellow pa5'>Quotes go here</h1>
 		</div>
 	);
---
+++ ryan/src/components/Streak.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Streak = () => {
+	return
+	(
+
+	);
+
+}
+
+export default Streak;
