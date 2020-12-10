f43431ad9b7c8db76963ed8872d33fb6243122de
KajalTomar <kajaltomar09@gmail.com>
2020-12-10 00:59:09 -0600

    just a background

---
+++ ryan/src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quote = () => {
+	return(
+		<div style={{display: 'flex', justifyContent: 'center'}}>
+			<h1 className='f2 dim yellow pa5'>Quotes go here</h1>
+		</div>
+	);
+}
+
+export default Quote;
