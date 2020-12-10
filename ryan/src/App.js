a01544e6a180d164f0d42c9d36b018b2d816a397
KajalTomar <kajaltomar09@gmail.com>
2020-12-10 13:54:45 -0600

    just in case

--- ryan/src/App.js
+++ ryan/src/App.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
 import './App.css'
 import Quote from './components/Quote';
-
+import Streak from './components/Streak';
 
 class App extends Component {
 	render(){
 		return (
 			<div className="App">
 				<Quote />
-				{ /*<Logo />
 				<Streak />
-				<Graph />
+				{ /* <Graph />
 				<Badges />
 				<Stats /> */ }
 			</div>
