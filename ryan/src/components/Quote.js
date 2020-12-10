4d8a19129c6bd86feff4e4751dc3d162b7536380
KajalTomar <kajaltomar09@gmail.com>
2020-12-10 14:28:44 -0600

    w

--- ryan/src/components/Quote.js
+++ ryan/src/components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Quote = () => {
 	return(
 		<div className="Quote" style={{display: 'flex', justifyContent: 'center'}}>
-			<h1 className='f4 dim yellow pt5 pb0'>Yo what is up over there bud</h1>
+			<h1 className='f4 dim white pt5 pb0'>Yo what is up over there bud</h1>
 		</div>
 	);
 }
--- ryan/src/components/Streak.js
+++ ryan/src/components/Streak.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import flame from './flame.gif';
+// import flame from './flame.gif';
 
 const Streak = () => {
 	return(
 		<div className="Streak" style={{display: 'flex', flexDirection:'column'}}>
-			<p className='f1 yellow mv0 pill b--washed-yellow'>100</p>
-			<img id="flame" src={flame} alt='imagine that there is a gif flame here'/>
+			<p className='f1 white mv0 pill b--washed-yellow'>100</p>
+			{/* <img id="flame" src={flame} alt='imagine that there is a gif flame here'/>*/}
 		</div>
 	);
 
