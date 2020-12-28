7cb8c13f6793c82cd329a6ca0dc21f200ea6a0e1
KajalTomar <kajaltomar09@gmail.com>
2020-12-28 15:10:44 -0600

    cool bg that ryan chose

--- ryan/src/App.js
+++ ryan/src/App.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import './App.css'
 import Quote from './components/Quote';
 import Streak from './components/Streak';
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 110,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		},
+		line_linked:{
+			color: {
+				value: "#FFECFF"
+			},
+			opacity: 0.8
+		}		
+	},
+	interactivity: {
+		detect_on: "window",
+		events: {
+			onhover: {
+				enable: true,
+				mode: "repulse"
+			}
+		},
+		modes: {
+			repulse: 500
+		}
+	}
+}
 
 class App extends Component {
 	render(){
 		return (
 			<div className="App">
+				<Particles className='particles'
+					params={particlesOptions}
+				/>
 				<Quote />
 				<Streak />
 				{ /* <Graph />
